16c1e5133ca258aeca5784a83a8587ee9098c7b3
eodevelop <eodevelop@naver.com>
2024-06-20 00:19:36 +0900

    라우팅 강의 마무리

--- nwitter-reloaded/src/App.tsx
+++ nwitter-reloaded/src/App.tsx
@@ -2,6 +2,10 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Layout from "./components/layout";
 import Home from "./routes/home";
 import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+import reset from "styled-reset";
+import { createGlobalStyle } from "styled-components";
 
 const router = createBrowserRouter(
   [
@@ -18,13 +22,35 @@ const router = createBrowserRouter(
           element: <Profile />,
         }
       ]
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/create-account",
+      element: <CreateAccount />,
     }
   ]
 );
 
+
+const GlobalStyle = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color: white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; 
+  }
+`;
+
 function App() {
   return <>
-    <RouterProvider router={router} />
+  <GlobalStyle />
+  <RouterProvider router={router} />
   </>;
 }
 
--- nwitter-reloaded/src/components/layout.tsx
+++ nwitter-reloaded/src/components/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout() {
     // Outlet 컴포넌트는 라우트 컴포넌트의 하위 라우트를 렌더링하는데 사용된다.
     return (
         <>
-            <h2>layout</h2>
+            <h2>layout</h2> {/* h2 layout으로 되있는 부분은 추후 navigation 으로 변경될 예정 */}
             <Outlet />
         </>
     )
