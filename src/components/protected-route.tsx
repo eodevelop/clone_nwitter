60e29e1154367d61c0fcfe736a7d50dc1114d70d
eodevelop <eodevelop@naver.com>
2024-06-30 14:17:26 +0900

    Protected Routes 관련 설명 추가

--- src/components/protected-route.tsx
+++ src/components/protected-route.tsx
@@ -1,6 +1,7 @@
 import {Navigate} from "react-router-dom";
 import {auth} from "../firebase";
 
+// ProtectedRoute 컴포넌트는 사용자가 로그인하지 않은 경우 로그인 페이지로 리다이렉트한다.
 export default function ProtectedRoute({
    children,
 }: {
