5c57453615a41f557ea3696d81ccdcfbf17eef91
eodevelop <eodevelop@naver.com>
2024-07-01 18:42:11 +0900

    #4.0 Navigation Bar 강의 완료

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,15 +1,3 @@
-import { auth } from '../firebase';
-import { useNavigate } from 'react-router-dom';
-
 export default function Home() {
-  const navigate = useNavigate();
-  const logOut = () => {
-    auth.signOut();
-    navigate('/login');
-  };
-  return (
-    <h1>
-      <button onClick={logOut}>Log Out</button>
-    </h1>
-  );
+  return <h1></h1>;
 }
